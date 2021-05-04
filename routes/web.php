dfce527be7b126580da1d201948db31ff00cffce
Rifqi IR <74804470+RestInReal@users.noreply.github.com>
2021-05-04 22:19:03 +0700

    Add files via upload

--- routes/web.php
+++ routes/web.php
@@ -12,6 +12,10 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
+Route::get('/praktikum-laravel', function () {
+    return view('Mahasiswa.index');
+});
+
 
 Route::get('/', function () {
     return view('welcome');
